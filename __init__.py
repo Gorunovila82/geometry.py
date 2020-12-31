09148f4a09282adc39f589b07e78279652a4ef85
Gorunovila82 <64749284+Gorunovila82@users.noreply.github.com>
2020-12-31 15:42:29 +0300

    Delete __init__.py

--- __init__.py
+++
@@ -1,6 +0,0 @@
-__doc__ = "geometry library"
-__package__ = "geometry.py"
-__version__ = "0.2"
-__github__ = "https://github.com/Gorunovila82/geometry.py/tree/main"
-PATH = __file__[:-11]
-print(PATH)
