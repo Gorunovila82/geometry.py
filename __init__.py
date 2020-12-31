2804a96be8bc1d0cf4a04b400538731f0a2b2f61
Gorunovila82 <64749284+Gorunovila82@users.noreply.github.com>
2020-12-31 12:13:02 +0300

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,6 @@
+__doc__ = "geometry library"
+__package__ = "geometry.py"
+__version__ = "0.2"
+__github__ = "https://github.com/Gorunovila82/geometry.py/tree/main"
+PATH = __file__[:-11]
+print(PATH)
