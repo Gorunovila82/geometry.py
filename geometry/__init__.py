27b29c7e18dd22422c76a0418d2c48ffd5f8f2cf
Ilya <gorunovila82@gmail.com>
2020-12-31 16:09:45 +0300

    2

--- geometry/__init__.py
+++ geometry/__init__.py
@@ -20,9 +20,9 @@ def _version_check():
     try:
         r = requests.get("https://api.github.com/repos/Gorunovila82/geometry.py/releases").json()[0]
         if __version__ != r["tag_name"]:
-            print("Присутствует новая весрсия " + r["tag_name"] + " от "+ r["author"]["login"] + "!") 
             if DOWNLOAD_NEW_VERSION:
                 _download(r)
+                print("Присутствует новая весрсия " + r["tag_name"] + " от "+ r["author"]["login"] + "!") 
 
     except:
         pass
